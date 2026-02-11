765ff8d81530a821a1584e790c7af149e5d7bb57
Draglon <s.nazarchuck@gmail.com>
2026-02-11 22:02:07 +0200

    Fix main section for desctop

--- src/views/Home/SectionMain/index.tsx
+++ src/views/Home/SectionMain/index.tsx
@@ -11,31 +11,33 @@ const SectionMain = () => {
 
   return (
     <section className="section section--main">
-      <Title className="section__title" level={1}>
-        <span className="section__title-line_1">
-          <span className="plane" />
-          {t("title.part_1")}
-        </span>
-        <span className="section__title-line_2">
-          {t("title.part_2")}
-        </span>
-      </Title>
-      <Title className="section__subTitle" level={2}>
-        {t("subTitle")}
-      </Title>
-      <form className="section__form">
-        <div className="section__field">
-          <Input className="section__input" placeholder={t("form.vacancy.placeholder")} />
-        </div>
-        <div className="section__field">
-          <Select className="section__select" placeholder={t("form.city.placeholder")} />
-        </div>
-        <div className="section__buttons">
-          <Button className="btn-search" htmlType="submit">
-            <i className="icon icon-search" />
-          </Button>
-        </div>
-      </form>
+      <div className="section__wrapper">
+        <Title className="section__title" level={1}>
+          <span className="section__title-line_1">
+            <span className="plane" />
+            {t("title.part_1")}
+          </span>
+          <span className="section__title-line_2">
+            {t("title.part_2")}
+          </span>
+        </Title>
+        <Title className="section__subTitle" level={2}>
+          {t("subTitle")}
+        </Title>
+        <form className="section__form">
+          <div className="section__field">
+            <Input className="section__input" placeholder={t("form.vacancy.placeholder")} />
+          </div>
+          <div className="section__field">
+            <Select className="section__select" placeholder={t("form.city.placeholder")} />
+          </div>
+          <div className="section__buttons">
+            <Button className="btn-search" htmlType="submit">
+              <i className="icon icon-search" />
+            </Button>
+          </div>
+        </form>
+      </div>
     </section>
   );
 };
